58c0998686cef4883b439600b5012c36951bf079
zannatulferdousianika <zfanika79@gmail.com>
2025-03-17 12:26:18 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
- function randomColor()
- 
- { 
-    document.body.style.backgroundColor=
-    'rgb('+ Math.round(Math.random() * 260)+
-    ','+ Math.round(Math.random() * 260)+
-    ','+ Math.round(Math.random() * 260)+')';
-     
- }
+function randomColor() { 
+    document.body.style.backgroundColor = 
+    'rgb(' + Math.round(Math.random() * 255) +
+    ',' + Math.round(Math.random() * 255) +
+    ',' + Math.round(Math.random() * 255) + ')';
+}
