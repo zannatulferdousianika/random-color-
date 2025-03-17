640779577f12e922b2f29edd6c55f2b05e164408
zannatulferdousianika <zfanika79@gmail.com>
2025-03-17 12:01:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ function randomColor()
+ 
+ { 
+    document.body.style.backgroundColor=
+    'rgb('+ Math.round(Math.random() * 260)+
+    ','+ Math.round(Math.random() * 260)+
+    ','+ Math.round(Math.random() * 260)+')';
+     
+ }
